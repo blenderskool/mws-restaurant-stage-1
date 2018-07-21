b0853095ec2051d670cb9822e3988ef868c18375
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-07-22 01:14:39 +0530

    feat: added gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+const htmlmin = require('gulp-htmlmin');
+
+/**
+ * Prepares the script files for production
+ */
+gulp.task('scripts', function() {
+
+  /**
+   * Bundles actual app scripts
+   */
+  gulp.src('src/js/*.js')
+    .pipe(concat('app.js'))
+    .pipe(babel())
+    .pipe(gulp.dest('dist/js'));
+
+  /**
+   * Use babel for service worker file
+   */
+  gulp.src('src/sw.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist'));
+});
+
+/**
+ * Prepares the style files for production
+ */
+gulp.task('styles', function () {
+  gulp.src('src/css/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('dist/css'));
+});
+
+/**
+ * Prepares the html files for production
+ */
+gulp.task('html', function() {
+  gulp.src('src/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeOptionalTags: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      useShortDoctype: true
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+/**
+ * Prepares the images for production
+ */
+gulp.task('images', function() {
+  gulp.src('src/img/**/*')
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('default', ['scripts', 'html', 'styles', 'images']);
